7c6fbfdc18b3addd980ff8de23e4596f38f9399c
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-01-25 16:04:44 -0300

    add Comment.jsx and Comment.module.css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ayhamyza.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Ayhamyza Ellen</strong>
+              <time title="25 de janeiro às 15:55" dateTime="2024-01-25 15:55:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
