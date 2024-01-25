94494871687189eccd5eb7e1b68687af29f1f745
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-01-25 17:39:13 -0300

    Comment Stylizations

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,32 +4,32 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment() {
   return (
-    <div className={styles.comment}>
-      <img src="https://github.com/ayhamyza.png" alt="" />
+        <div className={styles.comment}>
+            <img src="https://github.com/ayhamyza.png" alt="" />
 
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <strong>Ayhamyza Ellen</strong>
-              <time title="25 de janeiro às 15:55" dateTime="2024-01-25 15:55:00">Cerca de 1h atrás</time>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Ayhamyza Ellen</strong>
+                            <time title="25 de janeiro às 15:55" dateTime="2024-01-25 15:55:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button className= { styles.trash } title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer className= { styles.commentFooter }>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
             </div>
-
-            <button title="Deletar comentário">
-              <Trash size={20} />
-            </button>
-          </header>
-
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-
-        <footer>
-          <button>
-            <ThumbsUp />
-            Aplaudir <span>20</span>
-          </button>
-        </footer>
-      </div>
-    </div>
   )
 }
