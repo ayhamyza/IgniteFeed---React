18a72afe8d9db1b2c4393a3a94050646aa1bb671
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-01-29 15:45:41 -0300

    Creating Avatar component and styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar.jsx';
 import { Comment } from './Comment.jsx';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/ayhamyza.png" />
+          <Avatar src="https://github.com/ayhamyza.png" />
           <div className={styles.authorInfo}>
             <strong>Ayhamyza</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import {PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 
 export function Sidebar () {
     return (
         <aside className= { styles.sidebar }>
-            <img
-            className= { styles.cover } 
+            <Avatar
             src='https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             />
             
