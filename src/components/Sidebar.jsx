df7373c5be328473cb884bffb27dbf74794290f1
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-01-29 15:47:17 -0300

    Sidebar adjustment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,14 @@ import { Avatar } from './Avatar'
 export function Sidebar () {
     return (
         <aside className= { styles.sidebar }>
-            <Avatar
+            <img
+            className= { styles.cover } 
             src='https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             />
             
             <div className= { styles.profile }>
 
-                <img 
-                className= { styles.avatar } 
+                <Avatar
                 src="https://github.com/ayhamyza.png"
                 />
 
